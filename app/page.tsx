427943eb553099559bf7c59bfae3b308390e5cfc
MalithaJeewaka <144432834+MalithaJeewaka@users.noreply.github.com>
2024-08-02 13:04:49 +0530

    shadcn setup done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return;
+  return (
+    <div>
+      <h1 className="text-white">Home</h1>
+      <Button className="text-white">Click me</Button>
+    </div>
+  );
 }
